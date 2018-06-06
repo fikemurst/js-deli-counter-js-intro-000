f38c2ae0c414f9001458afbab458cceee80938b7
michael furst <michael@furst.us>
2018-06-06 12:33:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,14 @@ function nowServing(katzDeli){
     }
 }
 
+var line = []
 function currentLine(katzDeli){
   let i = 0
   while (i < katzDeli.length){
     line.push([i+1] + `. ` + katzDeli[i])
     i++
   }
-  if (katzDeliLine === 0){
+  if (katzDeli === 0){
     return "The line is currently empty."
   }
     else {
